730c1fcc4bed7722876d883925b33b85fb201f15
Dana Nikolaeva Chambourova <dnicolaeva@hotmail.com>
2015-04-11 13:54:02 -0500

    login stuff frist commit

---
+++ admin/template_content/receipts.php
@@ -0,0 +1,382 @@
+<?php
+    include_once 'php/db-connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mosaddek">
+    <meta name="keyword" content="FlatLab, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+    <link rel="shortcut icon" href="img/favicon.png">
+
+    <title>Bamboo | Receipt Reducer</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+
+    <!--right slidebar-->
+    <link href="css/slidebars.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+    <link href="css/mystyle.css" rel="stylesheet">
+    <link href="css/receipts.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+  <section id="container" class="">
+      <!--header start-->
+      <header class="header white-bg">
+          <div class="sidebar-toggle-box">
+              <div data-original-title="Toggle Navigation" data-placement="right" class="fa fa-bars tooltips"></div>
+          </div>
+          <!--logo start-->
+          <a href="index.html" class="logo" >Bam<span>Boo</span></a>
+          <!--logo end-->
+          <div class="nav notify-row" id="top_menu">
+            <!--  notification start -->
+            <ul class="nav top-menu">
+              <!-- settings start -->
+              <!-- settings end -->
+              <!-- inbox dropdown start-->
+              <li id="header_inbox_bar" class="dropdown">
+                  <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                      <i class="fa fa-envelope-o"></i>
+                      <span class="badge bg-important">2</span>
+                  </a>
+                  <ul class="dropdown-menu extended inbox">
+                      <div class="notify-arrow notify-arrow-red"></div>
+                      <li>
+                          <p class="red">You have 2 new messages</p>
+                      </li>
+                      <li>
+                          <a href="#">
+                              <span class="photo"><img alt="avatar" src="./img/avatar-mini.jpg"></span>
+                                    <span class="subject">
+                                    <span class="from">Jonathan Smith</span>
+                                    <span class="time">Just now</span>
+                                    </span>
+                                    <span class="message">
+                                        Hello, this is an example msg.
+                                    </span>
+                          </a>
+                      </li>
+
+                      <li>
+                          <a href="#">
+                              <span class="photo"><img alt="avatar" src="./img/avatar-mini4.jpg"></span>
+                                    <span class="subject">
+                                    <span class="from">Jondi Rose</span>
+                                    <span class="time">Just now</span>
+                                    </span>
+                                    <span class="message">
+                                        Hello, this is metrolab
+                                    </span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="#">See all messages</a>
+                      </li>
+                  </ul>
+              </li>
+              <!-- inbox dropdown end -->
+              <!-- notification dropdown start-->
+              <li id="header_notification_bar" class="dropdown">
+                  <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+
+                      <i class="fa fa-bell-o"></i>
+                      <span class="badge bg-warning">7</span>
+                  </a>
+                  <ul class="dropdown-menu extended notification">
+                      <div class="notify-arrow notify-arrow-yellow"></div>
+                      <li>
+                          <p class="yellow">You have 7 new notifications</p>
+                      </li>
+                      <li>
+                          <a href="#">
+                              <span class="label label-danger"><i class="fa fa-bolt"></i></span>
+                              Server #3 overloaded.
+                              <span class="small italic">34 mins</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="#">
+                              <span class="label label-warning"><i class="fa fa-bell"></i></span>
+                              Server #10 not respoding.
+                              <span class="small italic">1 Hours</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="#">
+                              <span class="label label-danger"><i class="fa fa-bolt"></i></span>
+                              Database overloaded 24%.
+                              <span class="small italic">4 hrs</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="#">
+                              <span class="label label-success"><i class="fa fa-plus"></i></span>
+                              New user registered.
+                              <span class="small italic">Just now</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="#">
+                              <span class="label label-info"><i class="fa fa-bullhorn"></i></span>
+                              Application error.
+                              <span class="small italic">10 mins</span>
+                          </a>
+                      </li>
+                      <li>
+                          <a href="#">See all notifications</a>
+                      </li>
+                  </ul>
+              </li>
+              <!-- notification dropdown end -->
+          </ul>
+          </div>
+          <div class="top-nav ">
+              <ul class="nav pull-right top-menu">
+                  <li>
+                      <input type="text" class="form-control search" placeholder="Search">
+                  </li>
+                  <!-- user login dropdown start-->
+                  <li class="dropdown">
+                      <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                          <img alt="" src="img/avatar1_small.jpg">
+                          <span class="username">Jhon Doue</span>
+                          <b class="caret"></b>
+                      </a>
+                      <ul class="dropdown-menu extended logout">
+                          <div class="log-arrow-up"></div>
+                          <li><a href="#"><i class=" fa fa-suitcase"></i>Profile</a></li>
+                          <li><a href="#"><i class="fa fa-cog"></i> Settings</a></li>
+                          <li><a href="#"><i class="fa fa-bell-o"></i> Notification</a></li>
+                          <li><a href="login.html"><i class="fa fa-key"></i> Log Out</a></li>
+                      </ul>
+                  </li>
+
+                  <!-- user login dropdown end -->
+                  <!-- start of button to open right menu-->
+                  <!-- end  of button to open right menu-->
+              </ul>
+          </div>
+      </header>
+      <!--header end-->
+      <!--sidebar start-->
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+                  <li>
+                      <a href="bamboo_index.html">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;">
+                          <i class="fa fa-file-text-o"></i>
+                          <span>View Receipts</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="receipts.html">All Receipts</a></li>
+                          <!--<li><a  href="horizontal_menu.html">Horizontal Menu</a></li>
+                          <li><a  href="header-color.html">Different Color Top bar</a></li>
+                          <li><a  href="mega_menu.html">Mega Menu</a></li>
+                          <li><a  href="language_switch_bar.html">Language Switch Bar</a></li>
+                          <li><a  href="email_template.html" target="_blank">Email Template</a></li>-->
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;">
+
+                          <i class="fa fa-bar-chart"></i>
+                          <span>Expenditure Analytics</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="boxed_page.html">Boxed Page</a></li>
+                          <li><a  href="horizontal_menu.html">Horizontal Menu</a></li>
+                          <li><a  href="header-color.html">Different Color Top bar</a></li>
+                          <li><a  href="mega_menu.html">Mega Menu</a></li>
+                          <li><a  href="language_switch_bar.html">Language Switch Bar</a></li>
+                          <li><a  href="email_template.html" target="_blank">Email Template</a></li>
+                      </ul>
+                  </li>
+
+                  <li>
+                      <a  href="login.html">
+                          <i class="fa fa-user"></i>
+                          <span>Logout</span>
+                      </a>
+                  </li>
+
+                  <!--multi level menu start-->
+                  <!--
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-sitemap"></i>
+                          <span>Multi level Menu</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="javascript:;">Menu Item 1</a></li>
+                          <li class="sub-menu">
+                              <a  href="boxed_page.html">Menu Item 2</a>
+                              <ul class="sub">
+                                  <li><a  href="javascript:;">Menu Item 2.1</a></li>
+                                  <li class="sub-menu">
+                                      <a  href="javascript:;">Menu Item 3</a>
+                                      <ul class="sub">
+                                          <li><a  href="javascript:;">Menu Item 3.1</a></li>
+                                          <li><a  href="javascript:;">Menu Item 3.2</a></li>
+                                      </ul>
+                                  </li>
+                              </ul>
+                          </li>
+                      </ul>
+                  </li>-->
+                  <!--multi level menu end-->
+
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
+      <!--sidebar end-->
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper site-min-height">
+              <!-- page start-->
+              <div class="body_header">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <button id="add_button" class="receipt_button float_left">Add Receipt</button>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" id="receipt_search" class="form-control search Float_right" placeholder="Search">
+                        </div>
+
+                    </div>
+                    <div class="body_title">
+                        My Receipts
+                    </div>
+
+              </div>
+              <div id = "body_receipts" class = "container-fluid">
+                <div class="row">
+                    <div class="col-md-3 col-sm-4 col-xs-12">
+                        <div class = "receipt_holder">
+                            <p class="receipt_store">
+                                WALMART
+                            </p>
+                            <p class="receipt_location">
+                                606 E Stoughton Champaign 21820
+                            </p>
+                            <p class="receipt_date">
+                                March 23rd 2015
+                            </p>
+                            <ul class="receipt_products">
+                                <li>
+                                    Milk 2,0
+                                </li>
+                                <li>
+                                    Tomato 3,40
+                                </li>
+                                <li>
+                                    Soup 1,45
+                                </li>
+                                <li>
+                                    Shampoo 5,34
+                                </li>
+                                <li>
+                                    Milk 2,0
+                                </li>
+                                <li>
+                                    Tomato 3,40
+                                </li>
+                                <li>
+                                    Soup 1,45
+                                </li>
+                                <li>
+                                    Shampoo 5,34
+                                </li>
+                            </ul>
+                            <p class="receipt_price">
+                                Pretax: 20$
+                            </p>
+                            <p class="receipt_price">
+                                Total Price: 25$
+                            </p>
+
+                          <button id="edit_button" class="receipt_button">Edit</button>
+                          <button id="delete_button" class="receipt_button" >Delete</button>
+
+                        </div>
+
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-12">
+                        <div class = "receipt_holder">
+                            .col-md-4
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-12">
+                        <div class = "receipt_holder">
+                            .col-md-4
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-12">
+                        <div class = "receipt_holder">
+                            .col-md-4
+                        </div>
+                    </div>
+                </div>
+              </div>
+              <!-- page end-->
+          </section>
+      </section>
+      <!--main content end-->
+
+      <!-- Right Slidebar start -->
+      <!-- Right Slidebar end -->
+
+      <!--footer start-->
+      <footer class="site-footer">
+          <div class="text-center">
+              2015 &copy; Bamboo Receipt Reducer
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
+      <!--footer end-->
+  </section>
+
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/slidebars.min.js"></script>
+    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="js/respond.min.js" ></script>
+
+    <!--common script for all pages-->
+    <script src="js/common-scripts.js"></script>
+
+
+
+  </body>
+</html>
